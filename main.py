ab440868890bc714e0edb51fd1f70db2be4b6d1d
mike b <mukeshbarnwal.iit@gmail.com>
2025-07-31 20:11:26 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+tasks = []
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "tasks": tasks})
+
+@app.post("/add")
+async def add_task(task: str = Form(...)):
+    tasks.append(task)
+    return RedirectResponse(url="/", status_code=303)
+
+@app.get("/delete/{task_id}")
+async def delete_task(task_id: int):
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
+    return RedirectResponse(url="/", status_code=303)
